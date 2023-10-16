928e7e5d2cafc34d27313a9c6b62a50f4dc1ff05
Tushar8640 <imrantushar44@gmail.com>
2023-10-17 03:11:43 +0600

    project setup completed

---
+++ src/redux/api/apiSlice.ts
@@ -0,0 +1,8 @@
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+//base for all api
+export const api = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({baseUrl: "http://localhost:5000/api/v1"}),
+  tagTypes: ["Products", "singleProduct", "cart", "order", "orders"],
+  endpoints: () => ({}),
+});
