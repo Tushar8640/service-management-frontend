52047b1fcee2b184ec1a4be9512158ef0c0bf30c
Tushar8640 <imrantushar44@gmail.com>
2023-10-19 20:34:27 +0600

    faq and feedback added

--- src/pages/dashboard/bookings/index.tsx
+++ src/pages/dashboard/bookings/index.tsx
@@ -1,5 +1,7 @@
 import BookingTable from "@/components/table/BookingTable";
+import UserTable from "@/components/table/UserTable";
 import DashboardLayout from "@/layouts/Admin";
+import {useGetBookingsQuery} from "@/redux/features/bookings/bookingApi";
 import React from "react";
 
 export default function users() {
